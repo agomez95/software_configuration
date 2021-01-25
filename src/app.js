82cc0523ec27cbf0fdd315d20b62a28eca268f19
agomez95 <sandmancity14@gmail.com>
2021-01-25 18:03:18 -0500

    se argego la configuracion para el modulo handlebars y el view engine de este: se seteo la carpeta de views y posteriormente se adeherieron como propiedades la carpetas layout y partials y la extension

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 //INITIALIZER
 const app = express();
@@ -9,6 +10,15 @@ const app = express();
 
 //SETTINGS
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs-engine', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', 'hbs-engine');
 
 //MIDDLEWARES
 app.use(morgan('dev'));
